cc5179d19e2c094b41013347b8edf28750f4e6fb
chengzheng <chengzheng@cmcm.com>
2018-11-13 20:41:36 +0800

    完成二分法求平方根

--- data_structure/binary_search/main.go
+++ data_structure/binary_search/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	list := []int{1,2,3,4,5,6,7,8,9}
@@ -31,14 +33,35 @@ func main() {
 	x = 5
 	fmt.Printf("search %d index:%d\n", x, binSearch(list, x))
 	fmt.Printf("recursive search %d index:%d\n", x, binSearchRecursive(list, 0, len(list)-1, x))
+
+
+	fmt.Println("====二分法求平方根====")
+	var (
+		number float32 = 2
+		threshold float32 = 0.000001
+	)
 	
+	ret, err := sqrtByBinSearch(number, threshold)
+	if err != nil {
+		fmt.Printf("sqrtByBinSearch(%f, %f) error(%v)\n", number, threshold, err)
+	} else {
+		fmt.Printf("sqrtByBinSearch(%f, %f) ret(%f)\n", number, threshold, ret)
+	}
+
+	number = 0.008
+	ret, err = sqrtByBinSearch(number, threshold)
+	if err != nil {
+		fmt.Printf("sqrtByBinSearch(%f, %f) error(%v)\n", number, threshold, err)
+	} else {
+		fmt.Printf("sqrtByBinSearch(%f, %f) ret(%f)\n", number, threshold, ret)
+	}
 }
 
 func binSearch(list []int, x int) int {
 	start := 0
 	end := len(list) - 1
 	for start <= end {
-		mid := start + (end - start) >> 1
+		mid := start + ((end - start) >> 1)
 		if list[mid] == x {
 			return mid
 		} else if list[mid] < x {
@@ -55,7 +78,7 @@ func binSearchRecursive(list []int, start, end, x int) int {
 		return -1
 	}
 
-	mid := start + (end - start) >> 1
+	mid := start + ((end - start) >> 1)
 	if list[mid] == x {
 		return mid
 	} else if list[mid] < x {
@@ -63,4 +86,38 @@ func binSearchRecursive(list []int, start, end, x int) int {
 	} else {
 		return binSearchRecursive(list, start, mid-1, x)
 	}
+}
+
+// 二分法求平方根
+// precision表示保留小数后多少位
+func sqrtByBinSearch(num, threshold float32) (float32, error) {
+	if num < 0 {
+		return 0.0, fmt.Errorf("invalid num:%v", num)
+	}
+	
+	var (
+		low float32 = 0.0
+		high = num
+	)
+	if num < 1 {
+		low = num
+		high = 1.0
+	}
+
+	mid := (low + high) / 2.0
+	for {
+		// 与原数据误差控制在threshold以内
+		dval := (mid * mid) - num
+		if dval > -1*threshold && dval < threshold {
+			break
+		}
+
+		if mid * mid > num {
+			high = mid
+		} else if mid * mid < num {
+			low = mid
+		}
+		mid = (low + high) / 2.0
+	}
+	return mid, nil
 }
