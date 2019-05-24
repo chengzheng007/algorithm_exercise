3644cf8804a736b5a6a9b71600f945164fdd3146
chengzheng <chengzheng@cmcm.com>
2019-05-24 10:32:39 +0800

    微调

--- jksj_exercise/hash_map_and_string/lru_cache.go
+++ jksj_exercise/hash_map_and_string/lru_cache.go
@@ -1,32 +1,68 @@
 package main
 
 import (
-	"container/list"
 	"errors"
 )
 
 var (
-	// ErrCacheSize indicate cache size must great than zero
-	ErrCacheSize = errors.New("invalid lru cache size")
+	// ErrCacheCap indicate cache size must great than zero
+	ErrCacheCap = errors.New("invalid lru cache capacity")
 )
 
 type lruCache struct {
-	size    int
-	hashMap map[string]int
-	dblist  *list.List
+	capacity int
+	count    int
+	hashMap map[string]*dblistNode
+	root  *dblistNode
 }
 
-func NewLruCache(size int) (*lruCache, error) {
-	if size <= 0 {
-		return nil, ErrCacheSize
+type dblistNode struct {
+	Val int
+	prev *dblistNode
+	next *dblistNode
+}
+
+func NewLruCache(capacity int) (*lruCache, error) {
+	if capacity <= 0 {
+		return nil, ErrCacheCap
 	}
-	return &lruCache{
-		size:    size,
-		hashMap: make(map[string]int),
-		dblist:  list.New(),
-	}, nil
+	lch := &lruCache{
+		capacity:    capacity,
+		hashMap: make(map[string]*dblistNode, capacity),
+		root: &dblistNode{},
+	}
+	lch.root.next = lch.root
+	lch.root.prev = lch.root
+	return lch, nil
 }
 
 func (lch *lruCache) Put(k string, v int) {
+	// 已存在，移动到最前面
+	if node, ok := lch.hashMap[k]; ok && node != nil {
 
+
+		node.prev.next = node.next
+		node.next.prev = node.prev
+		return
+	}
+	
+}
+
+// 将node节点添加到root节点后
+func (lch *lruCache) addNode(node *dblistNode) {
+	node.next = lch.root.next
+	node.prev = lch.root
+	lch.root.next = node
+	if lch.root.prev == lch.root {
+		lch.root.prev = node
+	}
+	lch.count++
+}
+
+func (lch *lruCache) remove(node *dblistNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+	node.prev = nil
+	node.next = nil
+	lch.count--
 }
