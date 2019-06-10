166236fbc9576b1606f5389bb4afb0012faf9f50
chengzheng <chengzheng@cmcm.com>
2019-06-10 12:18:33 +0800

    微调

---
+++ jksj_exercise/heap_and_bintree/heap_sort.go
@@ -0,0 +1,49 @@
+package main
+
+// size of list equal to (n+1)
+// big heap
+func buildHeap(list []int, n int) {
+	// 只有前一半有叶子节点，只需调整前一半
+	// 倒序遍历前一半节点，从上往下堆化
+	for i := n/2; i >= 1; i-- {
+		heapifyTopToBottom(list, n, i)
+	}
+}
+
+func heapifyTopToBottom(list []int, n, i int) {
+	for {
+		maxPos := i
+		if 2*i <= n && list[2*i] > list[i] {
+			maxPos = 2*i
+		}
+		if 2*i+1 <= n && list[2*i+1] > list[maxPos] {
+			maxPos = 2*i+1
+		}
+		// satisfy heap condition
+		if i == maxPos {
+			break
+		}
+		// swap
+		list[i], list[maxPos] = list[maxPos], list[i]
+
+		i = maxPos
+	}
+}
+
+func heapSort(list []int, n int) {
+	heapList := make([]int, n+1)
+	copy(heapList[1:], list)
+
+	// 建堆
+	buildHeap(heapList, n)
+
+	i := n
+	for i > 1 {
+		heapList[1], heapList[i] = heapList[i], heapList[1]
+		i--
+		// 因为与堆顶交换，从堆顶开始需要再做一次堆化
+		heapifyTopToBottom(heapList, i, 1)
+	}
+	// copy back
+	copy(list, heapList[1:])
+}
---
+++ jksj_exercise/heap_and_bintree/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := []int{4,6,1,7,2,9,5,8,3}
+	heapSort(list, len(list))
+	fmt.Printf("list:%v\n", list)
+}
