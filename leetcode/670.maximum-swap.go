a3a38b948013136c90effd73b5121e66f188d50a
chengzheng <chengzheng@cmcm.com>
2019-07-19 14:29:37 +0800

    微调

--- leetcode/670.maximum-swap.go
+++ leetcode/670.maximum-swap.go
@@ -4,37 +4,44 @@
  * [670] Maximum Swap
  */
 func maximumSwap(num int) int {
+	// 参考 solution Approach #2
 	list := make([]int, 0)
 	temp := num
 	for temp > 0 {
 		list = append(list, temp%10)
 		temp /= 10
-	}
+	} // 注意:list为数字的逆序排列
+	// 翻转后变为顺序
 	size := len(list)
-	maxNum := num
-	// 题目是要求最终结果是最多交换一次的结果，并不是我们交换一次就完事了，因为
-	// 直叫唤一次后的结果不一定是最大的，审题时要注意
-	// 暴力枚举，O(n^3)，注意还要从list获取
-	// 一个整数，需要遍历整个list，所以是n的3次方
-	// 参考leetcode solution Approach #2，非常有特点的解法，复杂度能到O(n)
-	for i := 0; i < size-1; i++ {
-		for j := i + 1; j < size; j++ {
-			list[i], list[j] = list[j], list[i]
-			curr := getNumFromList(list)
-			if curr > maxNum {
-				maxNum = curr
+	for i := 0; i < size/2; i++ {
+		list[i], list[size-i-1] = list[size-i-1], list[i]
+	}
+
+	// last数组: key为num数字中单个数，value为该数位于num中的索引位置
+	last := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		last[i] = -1
+	}
+	// 核心
+	for i := 0; i < size; i++ {
+		last[list[i]] = i
+	}
+
+	for i := 0; i < size; i++ {
+		for d := 9; d > list[i]; d-- {
+			if last[d] > i {
+				list[i], list[last[d]] = list[last[d]], list[i]
+				return getNumFromList(list)
 			}
-			// 交换后再交换回来，保证实际只发生一次交换
-			list[i], list[j] = list[j], list[i]
 		}
 	}
-	return maxNum
+	return num
 }
 
 func getNumFromList(list []int) int {
 	unit := 1
 	num := 0
-	for i := 0; i < len(list); i++ {
+	for i := len(list) - 1; i >= 0; i-- {
 		num += list[i] * unit
 		unit *= 10
 	}
