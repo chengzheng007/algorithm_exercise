777e6ecf8d2144ca7f04487a5f4687edff4acc05
chengzheng <chengzheng@cmcm.com>
2019-07-24 11:58:27 +0800

    添加注释

--- leetcode/55.jump-game.go
+++ leetcode/55.jump-game.go
@@ -6,10 +6,15 @@
 func canJump(nums []int) bool {
 	size := len(nums)
 	lastPos := size - 1
+	// 从最后一个位置往前找
 	for i := size - 1; i >= 0; i-- {
+		// 如果当前位置下标+加上当前位置可跳跃步数
+		// 大于等于当前位置值，说明在i处可达lastPos
+		// 注意当前下标一定可以到达当前下标，哪怕其对应的步数为0
 		if i+nums[i] >= lastPos {
 			lastPos = i
 		}
 	}
+	// 如果最后的位置可达，lastPos一定会被减小到0
 	return lastPos == 0
 }
