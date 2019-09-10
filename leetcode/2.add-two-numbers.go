413cd6c29cfb3e49299f177c7c595cf1dabb3bf5
chengzheng <chengzheng@cmcm.com>
2019-09-10 11:58:02 +0800

    添加注释

--- leetcode/2.add-two-numbers.go
+++ leetcode/2.add-two-numbers.go
@@ -11,34 +11,39 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	head := new(ListNode)
-	curr := head
-	var num1, num2, carry int
 	p := l1
 	q := l2
+	carry := 0
+	newList := new(ListNode)
+	curr := newList
 	for p != nil || q != nil {
-		num1 = 0
+		num1 := 0
 		if p != nil {
 			num1 = p.Val
-			p = p.Next
 		}
-		num2 = 0
+		num2 := 0
 		if q != nil {
 			num2 = q.Val
-			q = q.Next
 		}
 		sum := num1 + num2 + carry
-		curr.Next = &ListNode{Val: sum % 10}
+
+		curr.Next = &ListNode{Val:sum%10}
 		curr = curr.Next
-		if sum >= 10 {
-			carry = 1
-			// 当最后两个数和>=10时，需要在最后再加一个节点
-			// 如果是中间节点>=10，这里的Next会被覆盖掉，是没有影响的
-			curr.Next = &ListNode{Val: 1}
-		} else {
-			carry = 0
+
+		carry = sum/10
+		// 如果两数的最后一位如果两数之和大于10，在链表最后需要再加一位
+		// 如果不是最后一位的和也>=10，则该数会被覆盖，可以忽略
+		if carry > 0 {
+			curr.Next = &ListNode{Val:carry}
+		}
+
+		if p != nil {
+			p = p.Next
+		}
+		if q != nil {
+			q = q.Next
 		}
 	}
-	return head.Next
+	return newList.Next
 }
 
