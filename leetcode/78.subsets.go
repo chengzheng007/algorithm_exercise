cbb9effa47931790e91ebb2b13d53dea46c1daac
chengzheng <chengzheng@cmcm.com>
2020-05-17 22:57:45 +0800

    增加子集和实现方法

--- leetcode/78.subsets.go
+++ leetcode/78.subsets.go
@@ -31,6 +31,14 @@ func subsets(nums []int) [][]int {
 // }
 
 // 回溯算法版本2-加一个循环实现
+// i		k		curSet		curSet2
+// 0		0		[]			[1]
+// 1		1		[1]			[1,2]
+// 2		2		[1,2]		[1,2,3]
+// 2		1		[1]			[1,3]   (由i=2/k=2递归回退，由i=1/k=1下一次for循环得来，i++后i=2)
+// 1		0		[]			[2]		(由i=2/k=1递归回退，由i=0/k=0下一次for循环得来，i++后i=1)
+// 2		2		[2]			[2,3]
+// 2		0		[]			[3]		(y由i=2/k=2递归回退，由i=1/k=0下一次for循环得来，i++后i=2)
 func backtrack2(nums []int, size, start int, currSet []int, sets *[][]int) {
 	*sets = append(*sets, currSet)
 	for i := start; i < size; i++ {
@@ -39,5 +47,25 @@ func backtrack2(nums []int, size, start int, currSet []int, sets *[][]int) {
 		currSet2[len(currSet)] = nums[i]
 		backtrack2(nums, size, i+1, currSet2, sets)
 	}
+}
+
+// 另一种方法：每一个数可选可不选，假设n为总个数
+// 则集合中肯定有2^n个元素（包含空集合）
+func subsets(nums []int) [][]int {
+	// 位运算
+	totalSetNum := 1 << len(nums)
+	totalSet := make([][]int, totalSetNum)
+	for i := 0; i < totalSetNum; i++ {
+		set := make([]int, 0)
+		// 100、010、001，跟三个数与运算，判断某个位置
+		// 的数是否在需要放在集合中
+		for j := 0; j < len(nums); j++ {
+			if i&(1<<j) != 0 {
+				set = append(set, nums[j])
+			}
+		}
+		totalSet[i] = set
+	}
+	return totalSet
 }
 
