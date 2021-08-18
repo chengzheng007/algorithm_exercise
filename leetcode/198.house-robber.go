d6deb2696705d05c82580ff68c2c482d1aeaead2
Zheng Cheng <chengzheng007@gmail.com>
2021-08-18 22:57:06 +0800

    Update 198.house-robber.go
    update leetcode 198

--- leetcode/198.house-robber.go
+++ leetcode/198.house-robber.go
@@ -1,27 +1,70 @@
 func rob(nums []int) int {
-	n := len(nums)
-	if n <= 0 {
-		return 0
-	}
-
-	dp := make([]int, n)
-	dp[0] = nums[0]
-
-	for i := 1; i < n; i++ {
-		// dp[i-2]+nums[i]表示不抢第i-1间屋子，抢劫第i间
-		// dp[i-1]表示抢第i-1间屋子的总金额，不能再抢第i间
-		// 比较二者谁大，取大的即可
-		// 进阶：最多保留i-2的数即可，不用全部使用
-		m := nums[i]
-		if i >= 2 {
-			m += dp[i-2]
-		}
-		if dp[i-1] > m {
-			dp[i] = dp[i-1]
-		} else {
-			dp[i] = m
-		}
-	}
-
-	return dp[n-1]
+    /*
+    if len(nums) == 0 {
+        return 0
+    }
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    dp := make([]int, len(nums)+1)
+    // 初始化状态，dp[1]收益最大是抢第一间房子
+    dp[1] = nums[0]
+    // dp[i]表示抢第i间房子累计最大金额
+    // 两种可能：1.不抢第i间，dp[i]=dp[i-1]
+    // 2.抢第i间，dp[i] = dp[i-2]+nums[i]
+    // 转移方程dp[i] = max{dp[i-1], dp[i-2]+nums[i-1]}
+    max := dp[0]
+    for i := 2; i <= len(nums); i++ {        
+        dp[i] = nums[i-1]+dp[i-2]
+        if dp[i-1] > dp[i] {
+            dp[i] = dp[i-1]
+        }
+        if dp[i] > max {
+            max = dp[i]
+        }
+    }
+    return max
+    */
+    
+    // 继续优化为无空间（空间压缩）
+    size := len(nums)
+    if size == 0 {
+        return 0
+    }
+    if size == 1 {
+        return nums[0]
+    }
+    
+    var pprev,prev,curr int
+    for i := 0; i < size; i++ {
+        if pprev+nums[i] > prev {
+            curr = pprev+nums[i]
+        } else {
+            curr = prev
+        }
+        pprev = prev
+        prev = curr
+    }
+    return curr
 }
+
+/*
+// dfs
+func rob(nums []int) int {
+    max := 0
+    helper(nums, 0, 0, &max)
+    return max
+}
+
+func helper(nums []int, k, curr int, max *int) {
+    if curr > *max {
+        *max = curr
+    }
+    if k >= len(nums) {
+        return
+    }
+    for i := k; i < len(nums); i++ {
+        helper(nums, i+2, curr+nums[i], max)
+    }
+}
+*/
