16c99fc5bc03dde6a9ac2d78d99240902fa4d5f1
Zheng Cheng <chengzheng007@gmail.com>
2021-08-26 12:16:01 +0800

    Update 153.find-minimum-in-rotated-sorted-array.go

--- leetcode/153.find-minimum-in-rotated-sorted-array.go
+++ leetcode/153.find-minimum-in-rotated-sorted-array.go
@@ -1,31 +1,17 @@
+// 参考 leetcode 33
 func findMin(nums []int) int {
-	low := 0
-	high := len(nums) - 1
-
-	for low < high {
-		mid := (low + high) / 2
-		if nums[mid] > nums[high] {
-			// 偏大的数字在左边占多数
-			low = mid + 1
-		} else if nums[mid] < nums[high] {
-			// 偏小的数字在右边并占多数
-			// 该情况下mid可能是最小的值，所以high赋值为mid
-			high = mid
-		} else {
-			// mid == high，如 [5, 0, 1, 1, 1, 1, 1]
-			// 最小值出现在mid左边
-			minVal := nums[low]
-			for i := low + 1; i <= mid; i++ {
-				if nums[i] > minVal {
-					minVal = nums[i]
-				} else {
-					minVal = nums[i]
-					break
-				}
-			}
-			return minVal
-		}
-	}
-
-	return nums[low]
+    // 不存在相同元素的写法
+    low := 0
+    high := len(nums)-1
+    for low < high {
+        mid := (low+high)/2
+        // 中点数值大于高点，说明左边已经有序，且左边数值偏大，往右移
+        // 反之，如果中点数值小于右边，说明右边是排好序的
+        if nums[mid] > nums[high] {
+            low = mid+1
+        } else {
+            high = mid
+        }
+    }
+    return nums[low]
 }
