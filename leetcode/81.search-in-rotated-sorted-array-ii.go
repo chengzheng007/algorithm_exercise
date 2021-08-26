4e4b4216f5d0702fa26b6519612c230eec0b945c
Zheng Cheng <chengzheng007@gmail.com>
2021-08-26 11:50:20 +0800

    Update 81.search-in-rotated-sorted-array-ii.go

--- leetcode/81.search-in-rotated-sorted-array-ii.go
+++ leetcode/81.search-in-rotated-sorted-array-ii.go
@@ -1,18 +1,31 @@
+// 参考33题题解，注意注释中else if细微差别，因为元素可能相同
 func search(nums []int, target int) bool {
-	return dfs(nums, 0, len(nums)-1, target)
-}
-
-// 直接递归搜索两边，而不是一边
-// 因为两边各自为递增序列，并且两边都可能有数字
-func dfs(nums []int, l, r, target int) bool {
-	if l > r {
-		return false
-	}
-
-	mid := (l + r) / 2
-	if nums[mid] == target {
-		return true
-	}
-
-	return dfs(nums, mid+1, r, target) || dfs(nums, l, mid-1, target)
+    length := len(nums)
+    low := 0
+    high := length-1
+    
+    for low <= high {
+        mid := (low+high)/2
+        if nums[mid] == target {
+            return true
+        }
+        
+        if nums[low] == nums[mid] {
+            low++
+	} else if nums[mid] <= nums[high] { // !!注意:此处须<=，否则报错
+            if target > nums[mid] && target <= nums[high] {
+                low = mid+1
+            } else {
+                high = mid-1
+            }
+        } else {
+            if target >= nums[low] && target < nums[mid] {
+                high = mid-1
+            } else {
+                low = mid+1
+            }
+        }
+    }
+    
+    return false
 }
