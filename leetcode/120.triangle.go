5d5a661223fae1e64c14cec35b3f9eb3b97569b2
Zheng Cheng <chengzheng007@gmail.com>
2021-08-27 13:57:46 +0800

    Update 120.triangle.go

--- leetcode/120.triangle.go
+++ leetcode/120.triangle.go
@@ -3,6 +3,38 @@
  *
  * [120] Triangle
  */
+
+// 从上到下的备忘录+递归方式，不够直观
+// bottom-up dp，minpath[k][i]表示第k层第i个元素的最小路径和
+// minpath[k][i] = min(minpath[k+1][i], minpath[k+1][i+1]) + triangle[k][i]
+// 由于k+1层只依赖第k层，可进一步压缩为，对于第k层
+// minpath[i] = min(minpath[i],minpath[i+1])+triangle[k][i]
+func minimumTotal(triangle [][]int) int {
+    n := len(triangle)
+    if n == 0 {
+        return 0
+    }
+    
+    minpath := make([]int, n)
+    // 初始化，最后一行作为dp初始值
+    for i := 0; i < n; i++ {
+        minpath[i] = triangle[n-1][i]
+    }
+    
+    // 从底向上
+    for layer := n-2; layer >= 0; layer-- {
+        for i := 0; i <= layer; i++ {
+            if minpath[i] < minpath[i+1] {
+                minpath[i] = minpath[i] + triangle[layer][i]
+            } else {
+                minpath[i] = minpath[i+1] + triangle[layer][i]
+            }
+        }
+    }
+    
+    return minpath[0]
+}
+
 func minimumTotal(triangle [][]int) int {
 	rows := len(triangle)
 	if rows == 1 {
