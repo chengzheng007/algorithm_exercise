6a172795b00acfa725b6e1b3496992abdb519db1
Zheng Cheng <chengzheng007@gmail.com>
2021-08-30 21:07:11 +0800

    Create 50.pow-x-n.go

---
+++ leetcode/50.pow-x-n.go
@@ -0,0 +1,18 @@
+func myPow(x float64, n int) float64 {
+    if n == 0 {
+        return 1
+    }
+    // 对于负指数优化
+    if n < 0 {
+        n = -n
+        x = 1/x
+    }
+    
+    // 偶数次幂
+    if n%2 == 0 {
+        return myPow(x*x, n/2)
+    }
+    
+    // 基数次幂
+    return x*myPow(x*x, n/2)
+}
