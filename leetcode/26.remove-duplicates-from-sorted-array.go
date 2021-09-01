43c9bcb1447c38d214f7aa33782505b6909f3b26
Zheng Cheng <chengzheng007@gmail.com>
2021-09-01 10:56:43 +0800

    Update 26.remove-duplicates-from-sorted-array.go

--- leetcode/26.remove-duplicates-from-sorted-array.go
+++ leetcode/26.remove-duplicates-from-sorted-array.go
@@ -4,41 +4,19 @@
  * [26] Remove Duplicates from Sorted Array
  */
 func removeDuplicates(nums []int) int {
-	size := len(nums)
-	if size <= 0 {
-		return 0
-	}
-
-	// var i, j int
-	// // 由于中间有j到末尾的移动，所以时间复杂度是O(n^2)
-	// // 参考solution，两个下标可以做到O(n)
-	// for i = 0; i < size; i++ {
-	// 	// 寻找与起始一段元素不同的索引位置
-	// 	for j = i + 1; j < size; j++ {
-	// 		if nums[j] != nums[i] {
-	// 			break
-	// 		}
-	// 	}
-	// 	// 将j开始的元素覆盖到i+1的位置
-	// 	if j > i+1 {
-	// 		for p := j; p < size; p++ {
-	// 			nums[p-(j-i)+1] = nums[p]
-	// 		}
-	// 		size -= j - i - 1
-	// 	}
-	// }
-	// return size
-
-	// 因为元素已排好序
-	// 将不重复的元素放置在已重复的元素后面，复杂度O(n)
-	// 覆盖已经重复元素后的第一个
-	i := 0
-	for j := 1; j < size; j++ {
-		if nums[i] != nums[j] {
-			i++
-			nums[i] = nums[j]
-		}
-	}
-	return i + 1
+    n := len(nums)
+    if n == 0 {   // 长度为0的单独处理
+        return 0
+    }
+    
+    index := 0  // index记录上一个相同元素的起始位置
+    for i := 1; i < n; i++ {
+        // 思想：一旦发现下一个不同的元素，覆盖到index后面的位置
+        if nums[i] != nums[index] { 
+            index++
+            nums[index] = nums[i]
+        }
+    }
+    // 返回唯一元素的个数
+    return index+1
 }
-
