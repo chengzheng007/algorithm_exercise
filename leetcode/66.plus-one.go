71c49eeb63ede4b6bd4eb930e15b86eea55eafd5
Zheng Cheng <chengzheng007@gmail.com>
2021-09-22 12:09:35 +0800

    Update 66.plus-one.go

--- leetcode/66.plus-one.go
+++ leetcode/66.plus-one.go
@@ -3,6 +3,34 @@
  *
  * [66] Plus One
  */
+// 更易理解的、直观的做法
+func plusOne(digits []int) []int {
+    carry := 0
+    n := len(digits)
+    // carry 表示进位，无需进位时carry为0，类似于两数相加的思想
+    for i := n-1; i >= 0; i-- {
+        newNum := digits[i]+carry
+        if i == n-1 {
+            newNum += 1
+        }
+        digits[i] = newNum%10
+        carry = newNum/10
+    }
+    
+    if carry == 0 {
+        return digits
+    }
+    
+    newDigits := make([]int, n+1)
+    newDigits[0] = carry
+    for i := 0; i < n; i++ {
+        newDigits[i+1] = digits[i]
+    }
+    
+    return newDigits
+}
+
+
 func plusOne(digits []int) []int {
 	if len(digits) == 0 {
 		return digits
